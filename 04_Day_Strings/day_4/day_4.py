0a82363885e855c64b7f3a1e43c3361cadb3cc8d
Kaden Nelson <kad_nel@outlook.com>
2026-05-22 16:21:48 -0400

    Completed Day 4 Exercises

---
+++ 04_Day_Strings/day_4/day_4.py
@@ -0,0 +1,18 @@
+# Question 1
+concatenate = "Thirty " + "Days " + "Of " + "Python "
+print(concatenate)
+
+# Question 3-5
+company = "Coding For All"
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(company.swapcase())
+
+# Question 9
+company_sliced = company[7:]
+print(company_sliced)
+
+# Question 10 
+print(company.index("Coding"))
